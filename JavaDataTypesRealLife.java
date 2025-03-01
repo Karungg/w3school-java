dc79e31091be5133fd1922b4bb61b442c359dd64
Karungg <miftahfadilah71@gmail.com>
2025-03-01 16:11:44 +0700

    Java data types real life

---
+++ JavaDataTypesRealLife.java
@@ -0,0 +1,13 @@
+public class JavaDataTypesRealLife {
+    public static void main(String[] args) {
+        String name = "Miftah";
+        float uts = 90.0f;
+        float uas = 95.0f;
+        float present = 100.0f;
+
+        double result = (.3 * uts) + (.5 * uas) + (.2 * present);
+
+        System.out.println("Name : " + name);
+        System.out.println("Grade : " + result);
+    }
+}
