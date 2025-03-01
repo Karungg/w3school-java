0074d60650ef492a5331bb52b7bdf3e3435734e1
Karungg <miftahfadilah71@gmail.com>
2025-03-01 15:47:42 +0700

    Java data types numbers

--- JavaDataTypesNumbers.java
+++ JavaDataTypesNumbers.java
@@ -1,6 +1,32 @@
 public class JavaDataTypesNumbers {
     public static void main(String[] args) {
-        byte myNum = 127;
-        System.out.println(myNum);
+        // Primitive numbers data types
+
+        // Byte can store -128 to 127
+        byte myByte = 127;
+        System.out.println(myByte);
+
+        // short can store -32768 to 32767
+        short myShort = 5000;
+        System.out.println(myShort);
+
+        // int can store -2147483648 to 2147483647
+        int myInt = 2147483647;
+        System.out.println(myInt);
+
+        // Long can store -9223372036854775808 to 9223372036854775807
+        long myLong = 9223372036854775807L;
+        System.out.println(myLong);
+
+        float myFloat = 3.85f;
+        System.out.println(myFloat);
+
+        double myDouble = 4.00d;
+        System.out.println(myDouble);
+
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
     }
 }
