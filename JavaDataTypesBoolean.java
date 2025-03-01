a658af3f9efc281588698c87e793d53f426e55f8
Karungg <miftahfadilah71@gmail.com>
2025-03-01 15:49:20 +0700

    Java data types boolean

---
+++ JavaDataTypesBoolean.java
@@ -0,0 +1,9 @@
+public class JavaDataTypesBoolean {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFacebookFun = true;
+
+        System.out.println(isJavaFun);
+        System.out.println(isFacebookFun);
+    }
+}
