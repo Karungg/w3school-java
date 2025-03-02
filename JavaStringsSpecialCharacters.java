c9c30f5db8cb4a9e3c727e7f9561a88cfb2f079b
Karungg <miftahfadilah71@gmail.com>
2025-03-02 09:03:17 +0700

    Java strings

---
+++ JavaStringsSpecialCharacters.java
@@ -0,0 +1,7 @@
+public class JavaStringsSpecialCharacters {
+    public static void main(String[] args) {
+        String txt = "\'Aku adalah seorang programmer handal\'";
+
+        System.out.println(txt);
+    }
+}
---
+++ JavaStringsStrings.java
@@ -0,0 +1,8 @@
+public class JavaStringsStrings {
+    public static void main(String[] args) {
+        String name = "Miftah Fadilah";
+        System.out.println(name.length());
+
+        System.out.println(name.indexOf("Fadilah"));
+    }
+}
