9e76e649f3f35a0277e8e08cb6fcb5cf3e04df72
Karungg <miftahfadilah71@gmail.com>
2025-03-02 13:15:53 +0700

    Java break and continue

---
+++ JavaBreakContinue.java
@@ -0,0 +1,21 @@
+public class JavaBreakContinue {
+    public static void main(String[] args) {
+        // for(int i = 0; i <= 10; i++) {
+        //     if (i == 4) {
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // }
+
+        int i = 0;
+
+        while (i < 10) {
+            if (i == 4) {
+                i++;
+                continue;
+            }
+            System.out.println(i);
+            i++;
+        }
+    }
+}
