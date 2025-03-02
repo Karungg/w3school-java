a464fa2c5335280782625855e63e2b612ce392a4
Karungg <miftahfadilah71@gmail.com>
2025-03-02 13:09:45 +0700

    Java loop

---
+++ JavaForEach.java
@@ -0,0 +1,9 @@
+public class JavaForEach {
+    public static void main(String[] args) {
+        String[] cars = {"Bugatti", "Koenigsegg", "Pagani"};
+
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
