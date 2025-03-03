187057d14a04a63b8095f9d93daf669161932a0b
Karungg <miftahfadilah71@gmail.com>
2025-03-03 14:14:28 +0700

    Java

--- JavaConstructor.java
+++ JavaConstructor.java
@@ -1,14 +1,17 @@
 public class JavaConstructor {
     String attrName;
     int attrAge;
+    String attrAddress;
 
-    public JavaConstructor(String name, int age) {
+    public JavaConstructor(String name, int age, String address) {
         attrName = name;
         attrAge = age;
+        attrAddress = address;
     }
 
     public static void main(String[] args) {
-        JavaConstructor obj1 = new JavaConstructor("Miftah Fadilah", 20);
+        JavaConstructor obj1 = new JavaConstructor("Miftah Fadilah", 20, "Indonesia");
         System.out.println("My name is " + obj1.attrName + "\nand my age is " + obj1.attrAge);
+        System.out.println(obj1.attrAddress);
     }
 }
