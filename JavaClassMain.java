efec85ecbf96d1848569590444d3c4bdc568914f
Karungg <miftahfadilah71@gmail.com>
2025-03-03 13:30:14 +0700

    Java class

---
+++ JavaClassMain.java
@@ -0,0 +1,3 @@
+public class JavaClassMain {
+    int x = 5;
+}
---
+++ JavaClassSecond.java
@@ -0,0 +1,6 @@
+public class JavaClassSecond {
+    public static void main(String[] args) {
+        JavaClassMain myObj = new JavaClassMain();
+        System.out.println(myObj.x);
+    }
+}
