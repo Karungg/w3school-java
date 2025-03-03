8da3e599f2279753e666a8b1071e4b71157c45b9
Karungg <miftahfadilah71@gmail.com>
2025-03-03 13:38:42 +0700

    Java class attribues

---
+++ JavaClassAttributes.java
@@ -0,0 +1,11 @@
+public class JavaClassAttributes {
+    int x;
+
+    public static void main(String[] args) {
+        JavaClassAttributes obj1 = new JavaClassAttributes();
+        JavaClassAttributes obj2 = new JavaClassAttributes();
+        obj1.x = 10;
+        obj2.x = 20;
+        System.out.println(obj1.x + obj2.x);
+    }
+}
